11b612079389213fc6530ae4fba88c5418321566
flahn <florian.lahn@googlemail.com>
2016-07-07 12:39:18 +0200

    - documentation of the regrid function

--- R/regrid.R
+++ R/regrid.R
@@ -1,6 +1,8 @@
 #' @include scidbst-class.R
 NULL
 
+# prepares the grid statement for the scidb call, which reflects the new resolution in dimension
+# indice units
 .prepareGrid = function (x, y) {
       grid = rep(1,length(dimensions(x)))
       names(grid) = dimensions(x)
@@ -51,11 +53,44 @@ NULL
   return(out)
 }
 
+#' Regrid / Resample operator for scidbst objects
+#'
+#' This function changes the resolution of certain dimensions of the scidbst object. 'Regrid' is in this case the targeted scidb operation and reflects
+#' a more general regrid approach. 'Resample' is based on the respective function of the raster package. The latter will simply perform a regrid on the
+#' spatial dimensions. The grid parameter reflects the number of cells per dimension that are used as a block for resampling. As for the current development
+#' of scidb, the aggregation functions are quite limited (min/max, average, sum, standard deviation) and do not support more elaborated function like bilinear
+#' interpolation.
+#'
+#' @note For information on the aggregation statements in scidb have a look on the supported AFL functions (\url{http://paradigm4.com/HTMLmanual/13.3/scidb_ug/ch12.html})
+#'
+#' @rdname resample-scidbst-methods
+#'
+#' @param x The scidbst object
+#' @param grid a vector of grid sizes having the same length as dimensions(x)
+#' @param expr (optional) aggregation function applied to every attribute on the grid, or a quoted SciDB aggregation expression
+#' @param y raster object
+#'
+#' @examples
+#' \dontrun{
+#' scidbconnect(...)
+#' scibdst.obj = scidbst(array_name)
+#' # 50x50 upscaling with scidb expression as aggregation function
+#' regridded = regrid(scidbst.obj,c(50,50,1),"avg(attribute1)") #resample (upscaling 50x50 dimension units, here dimensions: y,x,time)
+#' # 50x50 upscaling with sum aggregation function
+#' regridded = regrid(scidbst.obj,c(50,50,1),sum) #resample (upscaling 50x50 dimension units, here dimensions: y,x,time)
+#'
+#' r = raster(scidbst.obj, nrows=300, ncols=200) # resample by setting the number of rows and columns of the target
+#' resampled = resample(scidbst.obj,r,"avg(attribute1)")
+#' }
+#'
+#' @return scidbst object with new resolution information (adapted affine transformation)
+#' @seealso \code{\link{regrid,scidb-method}} for original regrid function, \code{\link{resample}} for the original resample function
 #' @export
 setMethod("regrid", signature(x="scidbst"), .regrid.scidbst)
 
+#' @rdname resample-scidbst-methods
 #' @export
-setMethod("resample", signature(x="scidbst", y="Raster"), function(x,y,...) {
+setMethod("resample", signature(x="scidbst", y="Raster"), function(x,y,expr) {
     grid = .prepareGrid(x,y)
-    return(.regrid.scidbst(x=x,grid=grid,...))
+    return(.regrid.scidbst(x=x,grid=grid,expr))
 })
