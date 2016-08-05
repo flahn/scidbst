42f8e99f99fc3c57dfe9275ab84b20fcf5e91f10
flahn <florian.lahn@googlemail.com>
2016-08-05 17:02:10 +0200

    - extended documentation on crop, subarray, cumulate - made subarray and cumulate generic (scidb functions need to be addressed with package name) - removed restriction in crop that array needs to have only the two spatial dimensions and implemented that behavior - removed bugs in subarray, where the extent was set wrong (ymax and ymin were switched)

--- R/subarray.R
+++ R/subarray.R
@@ -1,31 +1,31 @@
-# if (!isGeneric("subarray")) {
-#   setGeneric("subarray", function(x, ...) standardGeneric("subarray"))
-# }
+if (!isGeneric("subarray")) {
+  setGeneric("subarray", function(x, ...) standardGeneric("subarray"))
+}
 
-#' @export
 subarray.scidbst = function (x, limits, between = FALSE) {
-  ndim = length(dimensions(s))
+  ndim = length(dimensions(x))
 
   if (is.character(limits)) {
     limits = gsub("\\*","Inf",limits)
     limits = as.numeric(unlist(strsplit(limits,",")))
   }
   #TODO limits = scidbst object
+  #TODO limits = raster or extent (equals raster::crop)
 
   if (length(limits) != 2*ndim) stop("limits do not match dimension description [#limits != (2*#dims)]")
   if (!between) { #subarray call
     if (x@isSpatial) {
       #image origin has been shifted towards limits
-      xindex = which(dimensions(x)==getXDim(x))
+      xindex = which(dimensions(x)==getXDim(x)) #get position of "x" values
       xvals = c(limits[xindex],limits[xindex+ndim]) #min/max for xdim
 
-      yindex = which(dimensions(x)==getYDim(y))
+      yindex = which(dimensions(x)==getYDim(x)) #position of "y" values
       yvals = c(limits[yindex],limits[yindex+ndim]) #min/max for ydim
 
       #calculate upper left coordinate (origin of image coordinate system)
-      ul = .transformToWorld(x@affine,xvals[1],yvals[2])
+      ul = .transformToWorld(x@affine,xvals[1],yvals[1]) #note: image coordinate system trans(i0) > trans(iEnd)
 
-      lr = .transformToWorld(x@affine,xvals[2],yvals[1]) #to get an extent
+      lr = .transformToWorld(x@affine,xvals[2],yvals[2]) #to get an extent
       newExtent = extent(ul[1],lr[1],lr[2],ul[2])
     }
 
@@ -69,11 +69,41 @@ subarray.scidbst = function (x, limits, between = FALSE) {
     }
   } #else leave as is since we do not change dimension values
 
-
-
-
-  #.transformToWorld
-  #.calculateDimIndices
-
   return(out)
 }
+
+#' Subarray function for scidbst object
+#'
+#' This function is based on the scidb subarray function. It will create a subset of the array based on the stated dimension limits. Based
+#' on the 'between' flag in scidb will be used the AFL function 'subarray' or 'between'. When 'between is set \code{TRUE}, then the spatial
+#' and/or temporal references will not be changed, because the dimension values are not recalculated. If 'subarray' is used in scidb, then
+#' the dimension values are shifted to the new lower boundaries, which requires an adaption of the references (if necessary).
+#'
+#' @param x scidbst array object
+#' @param limits vector of coordinate ranges or a character string (see Details)
+#' @param between (logical) Whether or not the \code{between} function shall be used in scidb instead of \code{subarray}
+#' @return scidbst array object with modified dimension references
+#'
+#' @details The \code{limits} parameter needs to be either a vector of numerics or characters or a character string. The vector needs to
+#' have two times the number of dimension as elements in it. This also applies to the character string, which shall contain the limits
+#' divided by commas (','). Examples: c(0,0,0,10,10,10); "0,0,0,10,10,10". Also unbounded dimension statements are supported. Use the
+#' asterisk ('*') to define an unbounded dimension value.
+#'
+#' @note The package will provide this function as a generic S4 function. This means that the original subarray method from
+#' the scidb package will not be recognized as S4 function. Use scidb::subarray to call the original method.
+#'
+#' @seealso \code{\link{subarray}}
+#'
+#' @examples
+#' \dontrun{
+#' scidbconnect(...)
+#' chicago = scidbst("chicago_sts")
+#' expression = c(501,285,2,901,585,3) #minimum values, then maximum values for all dimensions
+#' expression2 = c("501","285","2","901","585","*")
+#' expression3 = "501,285,2,901,585,3"
+#'
+#' chicago_sub = subarray(chicago,expression,between=FALSE)
+#' }
+#'
+#' @export
+setMethod("subarray",signature(x="scidbst"), subarray.scidbst)
