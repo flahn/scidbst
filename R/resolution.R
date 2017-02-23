06a9440f49c15b5c778141c8be0503d3fc8788a1
flahn <florian.lahn@googlemail.com>
2017-02-23 14:39:03 +0100

    [documentation] - grouped schema related methods - changes in ncol/nrow
    [code]
    - removed unused and relicted method names()

--- R/resolution.R
+++ R/resolution.R
@@ -4,12 +4,19 @@ NULL
 #' Spatial resolution
 #'
 #' Returns information about the spatial resolution as numeric values, e.g.(xres,yres), xres or yres or in terms of number of
-#' cells (ncol, nrow).
+#' cells in West-East dimension and South-North dimension (ncol, nrow).
 #'
 #' The spatial resolution is calculated with the spatial extent and the number of rows or columns. The number of rows and columns
 #' can be derived by transforming the spatial extent with affine transformation into the dimensional index values and calculating
 #' the differences between the minimum and maximum value for each dimension.
 #' Therefore the spatial resolution describes the distances between the regular parallel lines throughout the image.
+#'
+#' In terms of the cell numbers for rows and columns, the array is considered to have 2 dimensions. ncol and nrow refer primarily
+#' to the spatial dimensions. If the array is spatial, then nrow and ncol refer to the number of cells in the
+#' North-South and the West-East axis respectively. If the array is just referenced with a temporal reference system, then nrow
+#' is 1 and ncol is the number of potential units in the temporal extent.
+#'
+#' In case both reference systems are present, then nrow and ncol only refer to the spatial dimensions.
 #'
 #' @name resolution,scidbst
 #' @aliases res,scidbst ncol,scidbst nrow,scidbst
