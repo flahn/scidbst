5b8ec4b4fc7bba0b8e33217257a97ba1ec694d1e
flahn <florian.lahn@googlemail.com>
2017-02-24 16:50:07 +0100

    [Documentation] - added a README file for GitHub - added a LICENSE file

--- R/pkg.R
+++ R/pkg.R
@@ -3,7 +3,7 @@
 #' This package extends the package scidb for spatial and temporal referenced dimensions. A set of functions were modified in order
 #' to maintain the correct references, when applying the functions in scidb.
 #'
-#' @name scidbst-package
+#' @name scidbst-pkg-package
 #'
 #' @note This package requires to SciDB instance to operate on, as well as the scidb package for R. It is also important that the SciDB instance
 #' runs the scidb4geo extension, which allows to maintain the references in scidb.
@@ -11,7 +11,7 @@
 #'
 #' @seealso
 #' The scidb4geo extension: \link{https://github.com/mappl/scidb4geo} \\
-#' Docker image for easy scidb4geo setup: \link{https://github.com/mappl/scidb-eo}
+#' Docker image for a quick scidb4geo setup: \link{https://github.com/mappl/scidb-eo}
 #'
 #' @author Florian Lahn <florian.lahn@uni-muenster.de>
 "_PACKAGE"
